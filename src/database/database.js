5b87185f4113e493c049de537231970adba728ba
Victor Augusto <victoraugustogoncalvesvalerio@gmail.com>
2026-07-10 00:50:17 -0300

    implement automatic database creation if it doesn't exist

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,18 @@
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
+import fs from "fs";
+import path from "path";
+
+const databaseDir = path.resolve("database");
+
+if (!fs.existsSync(databaseDir)) {
+    fs.mkdirSync(databaseDir, { recursive: true });
+}
+
+const databasePath = path.join(databaseDir, "usage.db");
+
 
 export default open({
-    filename: "./database/usage.db",
+    filename: databasePath,
     driver: sqlite3.Database
 });
